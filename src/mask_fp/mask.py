8791ac065f142ceda17f620651112c9a0eb29efa
A-4S <dev.forsythe@gmail.com>
2025-04-07 23:00:14 +0000

    Updated mask.py; 1. added params: assigned, updated 2. formatted 3. added docstring placeholder

--- src/mask_fp/mask.py
+++ src/mask_fp/mask.py
@@ -1,31 +1,41 @@
-from functools import wraps
+from functools import WRAPPER_ASSIGNMENTS, WRAPPER_UPDATES, wraps
 from inspect import BoundArguments, signature
-from typing import Any, Callable
+from typing import Any, Callable, Iterable, Literal
 
 from pipe_fp import pipe
 
 
-def mask[**P](f1: Callable[P, Any]):
-    def outer_wrapper[R](f2: Callable[..., R]) -> Callable[P, R]:
-        @wraps(f1)
+def mask[**P](
+    wrapped: Callable[P, Any],
+    assigned: Iterable[
+        Literal[
+            "__module__",
+            "__name__",
+            "__qualname__",
+            "__doc__",
+            "__annotations__",
+            "__type_params__",
+        ]
+    ] = WRAPPER_ASSIGNMENTS,
+    updated: Iterable[Literal["__dict__"]] = WRAPPER_UPDATES,
+):
+    """
+    mask docstring 📄
+    """
+
+    def outer_wrapper[R](f: Callable[..., R]) -> Callable[P, R]:
+        @wraps(wrapped, assigned, updated)
         def inner_wrapper(*args: P.args, **kwargs: P.kwargs) -> R:
             def bind_partial(f: Callable[P, Any]):
                 return signature(f).bind_partial(*args, **kwargs)
 
-
             def apply_defaults(ba: BoundArguments):
                 return ba.apply_defaults() or ba
 
-
             def callback(ba: BoundArguments):
-                return f2(*ba.args, **ba.kwargs)
+                return f(*ba.args, **ba.kwargs)
 
-
-            return pipe(
-                bind_partial,
-                apply_defaults,
-                callback
-            )(f1)
+            return pipe(bind_partial, apply_defaults, callback)(wrapped)
 
         return inner_wrapper
 
