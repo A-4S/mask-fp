9f653d2e1fb724899c79c77ce2b31d7151b94350
A-4S <dev.forsythe@gmail.com>
2025-04-08 21:21:36 +0000

    Created library directory

--- src/mask_fp/__init__.py
+++ src/mask_fp/__init__.py
@@ -1,3 +1,1 @@
 from .mask import mask as mask
-from .merge_annotations import merge_annotations as merge_annotations
-from .set_return_annotations import set_return_annotations as set_return_annotations
---
+++ src/mask_fp/library/__init__.py
@@ -0,0 +1,2 @@
+from .merge_annotations import merge_annotations as merge_annotations
+from .set_return_annotations import set_return_annotations as set_return_annotations
