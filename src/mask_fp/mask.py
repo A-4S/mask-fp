a185721104f842ece00cecf8cfbbdeabdc804d8b
A-4S <dev.forsythe@gmail.com>
2025-04-08 21:23:16 +0000

    Fixed mask.py; now sets return annotations

--- src/mask_fp/mask.py
+++ src/mask_fp/mask.py
@@ -1,8 +1,10 @@
 from functools import WRAPPER_ASSIGNMENTS, WRAPPER_UPDATES, wraps
-from inspect import BoundArguments, signature
+from inspect import BoundArguments, Signature, signature
 from typing import Any, Callable, Iterable, Literal
 
 from pipe_fp import pipe
+
+from .library import merge_annotations, set_return_annotations
 
 
 def mask[**P](
@@ -23,20 +25,24 @@ def mask[**P](
     mask docstring 📄
     """
 
-    def outer_wrapper[R](f: Callable[..., R]) -> Callable[P, R]:
+    def outer_wrapper[R](wrapper: Callable[..., R]) -> Callable[P, R]:
         @wraps(wrapped, assigned, updated)
         def inner_wrapper(*args: P.args, **kwargs: P.kwargs) -> R:
-            def bind_partial(f: Callable[P, Any]):
-                return signature(f).bind_partial(*args, **kwargs)
+            def bind_partial(s: Signature):
+                return s.bind_partial(*args, **kwargs)
 
             def apply_defaults(ba: BoundArguments):
                 return ba.apply_defaults() or ba
 
             def callback(ba: BoundArguments):
-                return f(*ba.args, **ba.kwargs)
+                return wrapper(*ba.args, **ba.kwargs)
 
-            return pipe(bind_partial, apply_defaults, callback)(wrapped)
+            return pipe(signature, bind_partial, apply_defaults, callback)(wrapped)
 
-        return inner_wrapper
+        return pipe(
+            lambda fs: [signature(f) for f in fs],
+            lambda signatures: merge_annotations(*signatures),
+            lambda merged_s: set_return_annotations(inner_wrapper, merged_s),
+        )((wrapped, wrapper)) or inner_wrapper
 
     return outer_wrapper
