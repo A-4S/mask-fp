12bd65509431965a73d142dbe9d2e574b47546c7
A-4S <dev.forsythe@gmail.com>
2025-04-13 14:41:00 +0000

    Updated types.py; added TypeAlias

--- src/mask_fp/type/__init__.py
+++ src/mask_fp/type/__init__.py
@@ -1,4 +1,7 @@
 from .types import (
     T as T,
-    P as P
+    P as P,
+    Wrapped as Wrapped,
+    Wrapper as Wrapper,
+    Masked as Masked,
 )
--- src/mask_fp/type/types.py
+++ src/mask_fp/type/types.py
@@ -1,6 +1,13 @@
-from typing import ParamSpec, TypeVar
+from collections.abc import Callable
+from typing import Any, ParamSpec, TypeAlias, TypeVar
 
 
 T = TypeVar("T")
 
 P = ParamSpec("P")
+
+Wrapped: TypeAlias = Callable[P, Any]
+
+Wrapper: TypeAlias = Callable[..., T]
+
+Masked: TypeAlias = Callable[P, T]
